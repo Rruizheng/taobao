fd04e8b042c77472aa2c8be1a01b6cf38fc865ce
Rruizheng <ruirui_zh@yeah.net>
2018-03-11 22:28:08 +0800

    top

--- js/main.js
+++ js/main.js
@@ -1,5 +1,62 @@
 window.onload = function()
 {
+	/*顶部标题栏	*/
+	(function top(){
+		var oArr = [];
+		var oNavBox = [];
+		for(var i=0;i<8;i++){
+			var oArri = document.getElementById("arrow"+i);
+			var oNavBoxi = document.getElementById("nav-box"+i);
+			oArr.push(oArri);
+			oNavBox.push(oNavBoxi);
+		}
+		for(var i=0;i<oArr.length;i++){
+			oArr[i].index = i;
+			oArr[i].onmouseover = function(){
+				for(var y in oNavBox) oNavBox[y].style.display = "none";
+				oNavBox[this.index].style.display = "block";
+				oNavBox[this.index].onmouseout = function(){
+					this.style.display = "none";
+				}
+			}
+			
+		}
+		
+	})();
+	
+	/*滚动搜索框*/
+	(function scrollSearch(){
+		
+		window.onscroll = function(){
+		    var oScrollSearch = document.getElementsByClassName("scroll-search-box")[0];
+		    var topScroll =document.body.scrollTop;
+			if(topScroll > 285){ //当滚动距离大于700px时执行下面的东西
+	          oScrollSearch.style.display="block";
+	        }else{//当滚动距离小于250的时候执行下面的内容，也就是让导航栏恢复原状
+	          oScrollSearch.style.display="none";
+	        }
+		}
+		var oScrollSearch = document.getElementsByClassName("scroll-search-box")[0];
+		var oScrollTitle = oScrollSearch.getElementsByClassName("current-title")[0];
+		var oScrollNav = oScrollSearch.getElementsByClassName("scroll-search-nav")[0];
+		var oScrollToggle = oScrollSearch.getElementsByClassName("scroll-search-toggle-box")[0];
+		var oLi = oScrollToggle.getElementsByTagName("li");
+		
+		oScrollNav.onmouseover=function(){
+			oScrollToggle.style.display = "block";
+		}
+		oScrollToggle.onmouseout=function(){
+			oScrollToggle.style.display = "none";
+		}
+		for(var i=1;i<oLi.length;i++){
+			oLi[i].index = i;
+			oLi[i].onclick = function(){
+				var oString = oScrollTitle.innerHTML;
+				oScrollTitle.innerHTML = this.innerHTML;
+				this.innerHTML = oString;
+			}
+		}	
+	})();
 	
 	/*头部搜索框*/
 	(function search(){
@@ -22,8 +79,6 @@ window.onload = function()
 				oDiv[this.index].className="selected";
 			};
 		}
-	})()
-	
-	
+	})();
 	
 }

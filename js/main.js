9372891bbf39cb6c23de51146304ce34828eceba
Rruizheng <ruirui_zh@yeah.net>
2018-03-16 22:00:16 +0800

    tupianlunbo

--- js/main.js
+++ js/main.js
@@ -1,32 +1,100 @@
 window.onload = function()
 {
+	
+	/*交换选择项目*/
+	function changeTitle(oLi1,oLi2){
+		oLi1.onclick = function(){
+			var oString = oLi2.innerHTML;
+			oLi2.innerHTML = this.innerHTML;
+			this.innerHTML = oString;
+		}
+	}
+	
+	function css(obj,attr,value){
+		//检验给函数的参数个数
+		switch(arguments.length){
+			case 2:
+			//例如css(oLi, {"opacity" : "0", "height" : "0"});设置了对象obj,第二个对象类型是object,批量设置属性。style[i]=attr[i]。attr[opacity] = 0。
+				if(typeof arguments[1] == "object"){
+					for(var i in attr) i == "opacity" ?(obj.style["filter"] = "alpha(opacity=" + attr[i] + ")") : obj.style[i] = attr[i];
+			//例如css(oLi, "height");第二个对象类型是string,读取属性
+				}else{
+					//IE用currentStyle方法，没有的话，用getComputedStyle方法
+					return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj,null)[attr]
+				}
+				break;
+			//有三个参数，单一设置属性。
+			case 3:
+				attr == "opacity" ? (obj.style["filter"] = "alpha(opacity=" + value + ")",obj.style[attr] = value/100) : obj.style[attr] = value;
+				break;
+		}
+	};
+	
 	/*顶部标题栏	*/
 	(function top(){
 		var oArr = [];
 		var oNavBox = [];
+		var oNavNone = [];
+		
+		oNavNone.push(document.getElementsByClassName("top-left4")[0]);
+		oNavNone.push(document.getElementsByClassName("top-right2")[0]);
+		oNavNone.push(document.getElementsByClassName("top-right4")[0]);
+		
+		/*滑动背景色*/
+		for(var i=0;i<oNavNone.length;i++){
+			oNavNone[i].index = i;
+			oNavNone[i].onmouseover = function(){
+				for(var y in oNavBox) oNavBox[y].style.display = "none";
+				for(var y in oArr) oArr[y].style.backgroundColor = "#f4f4f4";
+			}
+		}
+		
 		for(var i=0;i<8;i++){
 			var oArri = document.getElementById("arrow"+i);
 			var oNavBoxi = document.getElementById("nav-box"+i);
 			oArr.push(oArri);
 			oNavBox.push(oNavBoxi);
 		}
+		
+		/*显示隐藏栏目*/
 		for(var i=0;i<oArr.length;i++){
 			oArr[i].index = i;
 			oArr[i].onmouseover = function(){
 				for(var y in oNavBox) oNavBox[y].style.display = "none";
-				oNavBox[this.index].style.display = "block";
-				oNavBox[this.index].onmouseout = function(){
+				oNavBox[this.index].style.display = "block";	
+				
+				/*隐藏栏定位*/
+				(this.index!=(oArr.length-1))&&(oNavBox[this.index].style.left = this.offsetLeft + "px");
+				oNavBox[this.index].index = this.index;
+				
+				oNavBox[this.index].onmouseleave = function(){
 					this.style.display = "none";
+					oArr[this.index].style.backgroundColor = "#f4f4f4";
+				}
+				for(var y=0;y<oNavBox.length;y++){
+					if(oNavBox[y].style.display === "none"){
+						oArr[y].style.backgroundColor = "#f4f4f4";
+					}else if(oNavBox[y].style.display === "block"){
+						oArr[y].style.backgroundColor = "#fff";
+					}
 				}
 			}
-			
 		}
+		
+		/*切换选择项*/
+		for(var i=3;i<7;i++){
+			var oLi = oNavBox[i].getElementsByTagName("li");
+			var oTitle = oArr[i].getElementsByTagName("a")[0];
+			for(var y=0;y<oLi.length;y++){
+				changeTitle(oLi[y],oTitle);	
+			}
+		}
+		
 		
 	})();
 	
 	/*滚动搜索框*/
 	(function scrollSearch(){
-		
 		window.onscroll = function(){
 		    var oScrollSearch = document.getElementsByClassName("scroll-search-box")[0];
 		    var topScroll =document.body.scrollTop;
@@ -50,13 +118,11 @@ window.onload = function()
 		}
 		for(var i=1;i<oLi.length;i++){
 			oLi[i].index = i;
-			oLi[i].onclick = function(){
-				var oString = oScrollTitle.innerHTML;
-				oScrollTitle.innerHTML = this.innerHTML;
-				this.innerHTML = oString;
-			}
+			changeTitle(oLi[i],oScrollTitle);
 		}	
 	})();
+	
+	
 	
 	/*头部搜索框*/
 	(function search(){
@@ -80,5 +146,4 @@ window.onload = function()
 			};
 		}
 	})();
-	
 }
